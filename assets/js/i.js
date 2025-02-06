29f557c37bb6ee47e1136e6acac52083fd5e6080
RephEllis <rephael.ellis@gmail.com>
2025-02-06 17:46:22 -0500

    Update i.js

--- assets/js/i.js
+++ assets/js/i.js
@@ -41,15 +41,15 @@ function processUrl(value, path) {
     url = `https://${url}`;
   }
 
-  //sessionStorage.setItem("GoUrl", __uv$config.encodeUrl(url));
+  sessionStorage.setItem("GoUrl", __uv$config.encodeUrl(url));
   const dy = localStorage.getItem("dy");
 
   if (dy === "true") {
-    //window.location.href = `/a/q/${__uv$config.encodeUrl(url)}`;
+    window.location.href = `/a/q/${__uv$config.encodeUrl(url)}`;
   } else if (path) {
     location.href = path;
   } else {
-    //window.location.href = `/a/${__uv$config.encodeUrl(url)}`;
+    window.location.href = `/a/${__uv$config.encodeUrl(url)}`;
   }
 }
 
@@ -62,7 +62,7 @@ function blank(value) {
 }
 
 function dy(value) {
-  //processUrl(value, `/a/q/${__uv$config.encodeUrl(value)}`);
+  processUrl(value, `/a/q/${__uv$config.encodeUrl(value)}`);
 }
 
 function isUrl(val = "") {
